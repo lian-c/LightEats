5338c8d1a8c975f055348354de8846e743f934a5
lian-c <lian.chan@telus.com>
2022-12-17 10:15:34 -0800

    Update schema for users and orders

--- db/schema/04_orders.sql
+++ db/schema/04_orders.sql
@@ -3,8 +3,9 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE
     orders(
         id SERIAL PRIMARY,
-        customerId INT REFERENCES users(id),
-        restaurantId INT REFERENCES restaurants(id),
+        customer_id INT REFERENCES users(id),
+        estimated_time SMALLINT,
         total INT,
-        status BOOLEAN
+        status BOOLEAN,
+        order_time TIMESTAMP
     );
