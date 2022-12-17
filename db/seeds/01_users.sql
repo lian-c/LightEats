2aa67afcf8145ddd4437633da913a92e64ea47fe
lian-c <lian.chan@telus.com>
2022-12-17 11:36:47 -0800

    Fix typo on users.sql schema and removed a line on seeds users.sql

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,38 +1,36 @@
 INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Devin Sanders', 'tristanjacobs@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-349-3299', 'admin');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Iva Harrison', 'allisonjackson@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-125-2216', 'admin');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Lloyd Jefferson', 'asherpoole@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-848-9923', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Dale Coleman', 'michaelgray@mail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '860-831-0563', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Alejandro Osborne', 'ariaatkinson@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '604-094-6464', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Nell Medina', 'juliansantos@aol.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-534-4891', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Estelle Walsh', 'elistanton@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-538-9031', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'Herbert Graves', 'emilyowen@live.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '979-918-2964', 'user');
     INSERT INTO users (
-    name, email, password, phone_number, role) 
+    name, email, password, phone_number, role)
     VALUES (
     'John Stevens', 'charliebattle@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.', '778-971-9498', 'user');
-    INSERT INTO users (
-    name, email, password, phone_number, role) 
