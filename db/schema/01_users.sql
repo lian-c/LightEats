1785ee3962af8c45ffcaf9456030c24e91aa9260
lian-c <lian.chan@telus.com>
2022-12-17 11:44:06 -0800

    fix boolean and word key missing

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -2,11 +2,11 @@ DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE
     users (
-        id SERIAL PRIMARY,
+        id SERIAL PRIMARY KEY,
         email VARCHAR(255) unique,
         password VARCHAR(255),
         name VARCHAR(255),
         phone_number VARCHAR(10) unique,
         role VARCHAR(255),
-        status BOOLEAN DEFAULT VALUE TRUE
+        status BOOLEAN DEFAULT VALUE 1
     );
