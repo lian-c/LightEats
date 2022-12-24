80c778a0f5c575757c813c1b872eeda99f4bff10
Raheel Bennett <raheel.bennett@telus.com>
2022-12-24 01:25:34 -0700

    add menu route and menu.ejs with sample pictures

--- routes/menu.js
+++ routes/menu.js
@@ -4,8 +4,33 @@ const {getMenu} = require('../db/queries/menu');
 
 router.get('/', (req, res) => {
   getMenu()
-    .then(menu => {
-      res.json({menu});
+    .then(menuItems => {
+      const pizzaItems = [];
+      const wingItems = [];
+      const dipItems = [];
+      const beverageItems = [];
+      for(let item of menuItems) {
+        if (item.description === 'pizza') {
+          pizzaItems.push(item);
+        } 
+        if (item.description === 'chicken wings') {
+          wingItems.push(item);
+        }
+        if (item.description === 'dipping sauce') {
+          dipItems.push(item);
+        }
+        if (item.description === 'beverage') {
+          beverageItems.push(item);
+        }
+      }
+      console.log(pizzaItems);
+      const templateVars = {
+        pizzas: pizzaItems,
+        wings: wingItems,
+        dips: dipItems,
+        beverages: beverageItems 
+      };
+      res.render("menu", templateVars);
     })
     .catch(err => {
       res
