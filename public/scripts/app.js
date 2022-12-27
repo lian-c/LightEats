4109d9462e92e321577bff80d9bc1254c6962c09
Raheel Bennett <raheel.bennett@telus.com>
2022-12-27 13:55:46 -0700

    add featured items display on home page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,17 @@ $(() => {
 
   $.get('/menu/featured')
     .then(featured => {
-      console.log("Response", featured);
+      $('main').append(`<div class="cards"></div>`);
+      
+      for (featureItem of featured) {
+        $('.cards').append(`
+        <div class="card">
+          <img src=${featureItem.food_photo_url}>
+          <strong>${featureItem.name}</strong>
+        </div>
+        `)
+      }
+
 
     });
 });
