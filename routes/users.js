7f27034d3cc72578241de094fd8ea89defe7e75e
Aingaran Thirunadarajah <email@aingaran.com>
2023-01-02 11:35:52 -0500

    added hero box to home page

--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,29 @@
  */
 
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('users');
-}); 
+// fake user data.
+
+const {users} = require('../db/fakeUserData');
+
+router.get('/login', (req, res) => {
+  res.render('user/login');
+});
+
+router.post("/login", (req, res) => {
+  const user = users.filter(user => user.email === req.body.email);
+
+  if (!user) {
+    res.status(401).json({ error: "Login error" });
+  }
+  req.session.userId = user.id;
+  res.status(200).json(user);
+});
+
+router.get("/profile", (req, res) => {
+  res.send(`Fake Profile Page for user ${req.session.userId}`);
+})
+
 
 module.exports = router;
