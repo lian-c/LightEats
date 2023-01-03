7e6ff7c146dcc905d153bae1b60cb469a48799e7
Raheel Bennett <raheel.bennett@telus.com>
2023-01-03 01:37:41 -0700

    login function will work with live database and set cookie

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,6 @@
 $(() => {
 
+ 
   $.get('/menu/featured')
     .then(featured => {
       $('main').append(`
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,17 @@
-/*
- * All routes for Users are defined here
- * Since this file is loaded in server.js into /users,
- *   these routes are mounted onto /users
- * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
- */
-
 const express = require('express');
 const router = express.Router();
 const { getUsers } = require('../db/queries/users');
 
+router.post("/logout", (req, res) => {
+  req.session.userId = "";
+  res.status(200).send(req.body);
+});
 
 router.get('/', (req, res) => {
   getUsers()
   .then(usersData => {
-    res.json(usersData)
+
+     res.json({userLoggedIn: req.session.userId, usersData });
   })
   .catch(err => {
     res
@@ -23,19 +21,11 @@ router.get('/', (req, res) => {
 });
 
 router.post("/login", (req, res) => {
-  console.log(req.body);
-  // const user = users.filter(user => user.email === req.body.email);
-
-  // if (!user) {
-  //   res.status(401).json({ error: "Login error" });
-  // }
   req.session.userId = req.body.userID;
-  res.status(200);
+  res.status(200).send(req.body);
 });
 
-// router.get("/profile", (req, res) => {
-//   res.send(`Fake Profile Page for user ${req.session.userId}`);
-// })
+
 
 
 module.exports = router;
