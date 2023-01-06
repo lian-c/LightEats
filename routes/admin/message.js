478b8c3afc4453200f7914ae984c21cecc0d18fd
Aingaran Thirunadarajah <aingaran.thirunadarajah@telus.com>
2023-01-06 12:21:42 -0500

    added twilio messaging

--- routes/admin/message.js
+++ routes/admin/message.js
@@ -1,10 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const db = require("../../db/connection");
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-const client = require('twilio')(accountSid, authToken);
-const {getAllMessages} = require("../../lib/admin/messages")
+const accountSid = "ACe5af4f0c5e555accf462cffd1e1d052e";
+const authToken = "bd62b88c7e96aeb79e2380ec79f1c579";
+const client = require("twilio")(accountSid, authToken);
+
+
+
+
+const { getAllMessages, getMessageById } = require("../../lib/admin/messages")
 
 
 
@@ -16,7 +20,7 @@ router.get("/", (req, res) => {
 })
 
 router.get("/all", (req, res) => {
-  return getAllMessages().then(result=>{
+  return getAllMessages().then(result => {
 
     const templateVars = {
       messages: result
@@ -26,13 +30,25 @@ router.get("/all", (req, res) => {
     res.render("admin/messages", templateVars)
 
   })
-  .catch(error=>console.log(error.message))
+    .catch(error => console.log(error.message))
 })
 
 
 router.get("/:id", (req, res) => {
   const messageId = req.params.id;
-  res.status(200).json({ messageId: messageId })
+  return getMessageById(messageId).then(result => {
+    const templateVars = {
+      message: result
+    }
+
+    console.log(templateVars)
+    res.render("admin/message", templateVars)
+
+  })
+    .catch(error => {
+      console.log(error.message);
+      res.send(error.message)
+    })
 })
 
 
@@ -41,6 +57,8 @@ router.post("/", (req, res) => {
   const messageContent = req.body.messageContent;
   const messageMethod = req.body.messageMethod;
 
+  const message = `RE: Order: ${orderId} -- ${messageContent}`
+
 
   // Get user contact details for order ID.
   const query = "SELECT * FROM orders JOIN users ON users.id = orders.user_id WHERE orders.id = $1";
@@ -48,32 +66,26 @@ router.post("/", (req, res) => {
 
   return db.query(query, values).then(result => {
 
-    if (messageMethod == "text") {
-      return res.send(result.rows[0].phone_number);
-    }
-    else {
-      return res.send(result.rows[0].email);
-    }
+    const phone_number = result.rows[0].phone_number;
 
-    // INSERT MESSAGE INTO DATABASE
-    const query = "INSERT INTO messages (orderId, content, status) VALUES ($1, $2, $3)";
-    const values = [orderId, messageContent, 'unread'];
+    client.messages
+      .create({ body: message, from: "+14195485361", to: phone_number })
+      .then(message => {
 
-    db.query(query, values).then(result => res.send(`Message sent`).catch(error => console.log(error.message)))
 
+        // INSERT MESSAGE INTO DATABASE
+        const query = "INSERT INTO messages (orderid, messagebody, status) VALUES ($1, $2, $3)";
+        const values = [orderId, message, 'sent'];
+
+        db.query(query, values)
+          .then(result => res.send(`Message sent`)
+            .catch(error => console.log(error.message)))
+
+      })
   })
-  .catch(error=>console.log(error.message))
+    .catch(error => console.log(error.message))
 
   res.send(req.body)
-
-
-  if (messageMethod === "email") {
-    // send via nodemailer
-  }
-
-  if (messageMethod === "text") {
-    // send via twillio
-  }
 
 
 
--- routes/admin/order.js
+++ routes/admin/order.js
@@ -2,6 +2,7 @@ const db = require("../../db/connection")
 const express = require("express");
 const router = express.Router();
 const { getOrderById, getAllOrders, getOrdersByStatus, getOrdersByUser, updateOrder } = require("../../lib/admin/orders");
+const {getMessagesByOrder} = require("../../lib/admin/messages");
 
 router.get("/", (req, res) => {
   res.status(200).json({ message: "Orders Route" })
@@ -38,15 +39,17 @@ router.get("/:id", (req, res) => {
   const orderId = req.params.id;
   // const query = "SELECT orders.id as orderId, orders.order_status as orderStatus, orders.order_time as orderTime, orders.completed_time as orderCompletedTime, users.id as userId, users.name as userName, menu_items.name as topping FROM orders JOIN users ON orders.user_id = users.id  JOIN order_items ON orders.id = order_items.order_id JOIN menu_items ON menu_items.id = order_items.item_id WHERE orders.id = $1";
 
-  const query = "SELECT menu_items.name as menuname, menu_items.id as menuid, users.name as username, users.id as userid,  price, food_photo_url, prep_time, order_status as orderstatus, order_time as ordertime, completed_time, orders.id as orderid FROM order_items INNER JOIN menu_items ON order_items.item_id=menu_items.id INNER JOIN orders ON order_id=orders.id INNER JOIN users ON users.id=orders.user_id WHERE order_id=$1"
+  const query = "SELECT menu_items.name as menuname, menu_items.id as menuid, users.name as username, users.id as userid,  price, food_photo_url, prep_time, order_status as orderstatus, order_time as ordertime, completed_time, orders.id as orderid FROM order_items INNER JOIN menu_items ON order_items.item_id=menu_items.id INNER JOIN orders ON order_id=orders.id INNER JOIN users ON users.id=orders.user_id  WHERE order_id=$1"
   const values = [orderId];
 
   return db.query(query, values)
     .then(result => {
       console.log(result.rowCount)
 
+
       const templateVars = {
-        order: result.rows
+        order: result.rows,
+        message: null
       }
 
 
@@ -58,7 +61,7 @@ router.get("/:id", (req, res) => {
         return res.status(200).render("admin/error", templateVars)
       }
 
-      console.log(templateVars)
+      
       return res.status(200).render("admin/order", templateVars)
     })
     .catch(error => { return res.status(400).json({ error: error.message }) })
